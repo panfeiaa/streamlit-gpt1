b784bbdb729cf6ac9cc4a918d9ca5ace78f60218
panfeiaa <32411310+panfeiaa@users.noreply.github.com>
2023-02-17 14:47:50 +0800

    Update edu9.py

--- edu9.py
+++ edu9.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import openai 
+import openai
 import os
 import smtplib
 import pdfkit
@@ -7,6 +7,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
+
 try:
     from google.protobuf.internal import api_implementation
 except ImportError:
@@ -14,13 +15,13 @@ except ImportError:
     from google.protobuf import api_implementation
 import jinja2
 
-
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 st.title('OpenAI GPT Answer Checker')
 
 question = st.text_input("Q: What is your question?")
 answer = st.text_input("A: What is the answer?")
 model_engine = "text-davinci-003"
+
 
 def generate_answer(question):
     completions = openai.Completion.create(
@@ -33,6 +34,7 @@ def generate_answer(question):
     )
     message = completions.choices[0].text
     return message
+
 
 def send_email(response):
     question = st.text_input("Q: What is your email,if you want receive the results?")
@@ -65,25 +67,27 @@ def send_email(response):
     text = MIMEText(body)
     msg.attach(text)
     # 将回答生成为 HTML 页面
-   template = jinja2.Template("""
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="utf-8">
-        <title>OpenAI API Response</title>
-    </head>
-    <body>
-        <p>{{ text }}</p>
-    </body>
-    </html>
-    """)
+
+
+    template = jinja2.Template("""
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <title>OpenAI API Response</title>
+        </head>
+        <body>
+            <p>{{ text }}</p>
+        </body>
+        </html>
+        """)
     html = template.render(text=message)
 
     # 使用 pdfkit 将 HTML 页面转换为 PDF 文件
     html_filename = "openai_response.html"
     with open(html_filename, "w") as f:
         f.write(html)
-        
+
     # 使用 pdfkit 将 HTML 文件转换为 PDF 文件
     pdf_filename = "openai_response.pdf"
     options = {
@@ -102,27 +106,24 @@ def send_email(response):
         attachment = MIMEApplication(f.read(), _subtype='pdf')
         attachment.add_header('Content-Disposition', 'attachment', filename=pdf_filename)
         msg.attach(attachment)
-        
+
     # 删除临时文件
     os.remove(html_filename)
-    
+
     # 发送邮件
     with smtplib.SMTP(smtp_server) as server:
         server.login(from_email, 'zdmpzrjrpqeqbgdc')
         server.sendmail(from_email, to_email, msg.as_string())
-    
-    
 
 if st.button('Check'):
     statement = f"Is  the answer  to {question} {answer}?"
     print(statement)
     response = generate_answer(statement)
-    
-#    if  'you are good' in response.lower() :
-#        st.success("Answer is correct")
-#    else:
-#        st.error("Answer is incorrect")
-#   
+
+    #    if  'you are good' in response.lower() :
+    #        st.success("Answer is correct")
+    #    else:
+    #        st.error("Answer is incorrect")
+    #
     st.write(f"GPT-3's answer:{response}")
     send_email(response)
-
