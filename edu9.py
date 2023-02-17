9c6e8516f0b908a6fa539fd52db460abaa7beced
panfeiaa <32411310+panfeiaa@users.noreply.github.com>
2023-02-17 22:43:58 +0800

    Update edu9.py

--- edu9.py
+++ edu9.py
@@ -5,7 +5,7 @@ import os
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-
+from email.utils import formataddr
 try:
     from google.protobuf.internal import api_implementation
 except ImportError:
@@ -18,6 +18,7 @@ st.title('OpenAI GPT Answer Checker')
 question = st.text_input("Q: What is your question?")
 answer = st.text_input("A: What is the answer?")
 model_engine = "text-davinci-003"
+
 
 
 def generate_answer(question):
@@ -37,24 +38,28 @@ def generate_answer(question):
 to_email = st.text_input("请输入收件人邮箱地址")
 # 接收邮件主题
 subject = st.text_input("请输入邮件主题")
-
-
+subject1=subject.encode('utf-8')
+# 邮件服务器地址
+smtp_server = 'smtp.163.com'
 def send_email(response):
     # 接收邮件正文
     body = response
 
-    # 邮件服务器地址
-    smtp_server = 'smtp.163.com'
-
     # 发件箱配置
     from_email = 'pf305243464@163.com'
 
-    email_info = [body, smtp_server, from_email]
-    return email_info
+    msg = MIMEText(body, 'plain', 'utf-8')
+
+    msg['From'] = formataddr(('Sender', from_email))
+    msg['To'] = formataddr(('Recipient',to_email ))
+    msg['Subject'] = subject
+
+
+    return msg
 
 
 
-if st.button('Check'):
+if st.button('Check and send email'):
     statement = f"Is  the answer  to {question} {answer}?"
     response = generate_answer(statement)
     #    if  'you are good' in response.lower() :
@@ -63,21 +68,16 @@ if st.button('Check'):
     #        st.error("Answer is incorrect")
     #
     st.write(f"GPT-3's answer:{response}")
-
-
-if st.button("发送邮件"):
     try:
         # 连接 SMTP 服务器，这里以 QQ 邮箱为例
-        server = smtplib.SMTP_SSL(send_email(generate_answer(question))[1], 465)
-        server.login(send_email(generate_answer(question))[2], "VRFQJIMZRCUFAPBB")  # 填写自己的邮箱和密码
-
+        server = smtplib.SMTP_SSL(smtp_server, 465)
+        server.login('pf305243464@163.com', "VRFQJIMZRCUFAPBB")  # 填写自己的邮箱和密码
         # 组装邮件内容
-        message = f"Subject: {subject}\n\n{send_email(generate_answer(question))[0]}"
-
+        message=send_email(response)
         # 发送邮件
-        server.sendmail(send_email(generate_answer(question))[2], to_email, message)
+        server.sendmail('pf305243464@163.com', to_email, message.as_string())
         server.quit()
-
         st.write(st.success("邮件已发送"))
     except Exception as e:
-        st.write(st.write(""))
+            st.write(st.error({e}))
+
